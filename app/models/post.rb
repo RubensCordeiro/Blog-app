2d52866df936d28baf1e7e47fd00d9afe21d4833
RubensCordeiro <rubensccordeiro@outlook.com>
2022-01-28 17:40:02 -0300

    Added model validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
+
   belongs_to :user
+  validates :title, presence: true, length: { in: 5..15 }
+  validates :summary, length: { maximum: 35 }
+  validates :description, presence: true, length: { in: 10..280 }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,19 @@
 class User < ApplicationRecord
-  has_many :posts
+  before_save { self.email = email.downcase }
+  before_save { self.name = name.capitalize }
+  before_save { self.surname = surname.capitalize }
+
+  has_many :posts, dependent: :destroy
   has_many :friendships
   has_many :friends, through: :friendships
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :name, presence: true, length: { maximum: 20}
+  validates :surname, presence: true, length: { maximum: 20}
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+                                    length: { maximum: 106 },
+                                    format: { with: VALID_EMAIL_REGEX }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
